89c4b764497d23925777192b504774a7c8559d4b
Olga Shevchenko <duvasu7777@gmail.com>
2022-01-17 22:31:46 +0200

    upd 1st task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const NOTIFICATION_DELAY = 1000;
+let timeoutId = null;
+const body = document.querySelector("body")
+console.log(body)
+const btnStart = document.querySelector("[data-start]")
+console.log(btnStart)
+const btnStop = document.querySelector("[data-stop]")
+console.log(btnStop)
 
+btnStart.addEventListener("click", event => {
+    timeoutId = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+        btnStart.disabled = true;
+        btnStop.disabled = false;
+    }, NOTIFICATION_DELAY)
+}
+)
+btnStop.addEventListener("click", event => {
+    btnStop.disabled = true;
+    btnStart.disabled = false;
+    clearInterval(timeoutId);
+    
+})
