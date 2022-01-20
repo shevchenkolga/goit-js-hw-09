24555c72690e912b8bf57a889908136e69faad8e
Olga Shevchenko <duvasu7777@gmail.com>
2022-01-20 21:33:04 +0200

    Task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
+import Notiflix from "notiflix";
+
+const formEl = document.querySelector(".form");
+const btnEl = document.querySelector("button");
+
+formEl.addEventListener("submit", onFormSubmit)
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
   if (shouldResolve) {
-    // Fulfill
+    resolve({ position, delay })
   } else {
-    // Reject
+    reject({ position, delay })
+  }
+    }, delay)
+    })
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  const formElements = evt.currentTarget.elements;
+  const delayOnStart = Number(formElements.delay.value);
+  const step = Number(formElements.step.value);
+  const amount = Number(formElements.amount.value);
+  let delayOnStep = 0;
+
+  for (let i = 0; i < amount; i += 1) {
+    delayOnStep = delayOnStart + i * step;
+
+    createPromise(i+1, delayOnStep)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
 }
+
